7b5b60a741881d89e9ba251a93c4446392098fbc
Saymun_khan <saymunkhan017890@gmail.com>
2023-04-26 14:04:29 +0600

    fifth2

--- components/Signup.js
+++ components/Signup.js
@@ -1,31 +1,36 @@
 import { Formik } from 'formik';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import React from 'react';
 import * as Yup from 'yup';
-import { Input } from '../../components/ContactForm';
-
-const account = () => {
+import { Input } from './ContactForm';
 
 
-  
+const Signup = () => {
   return (
-
-  <div className='p-20 max-sm:p-1 max-sm:max-w-sm max-sm:mx-auto'>
-
-    <div className="p-10 bg-white w-1/2 ml-64 max-sm:ml-0 max-sm:w-11/12 mt-10">
+    <div>
+        <div>
+            welcome 
+        </div>
+        <div className='p-20 max-sm:p-1 max-sm:max-w-sm max-sm:mx-auto'>
+    <div className="p-10 bg-white w-1/2 ml-64 max-sm:ml-0 max-sm:w-11/12">
     <div className="ml-10 max-sm:ml-0">
-        <h1 className="mb-3 text-3xl font-semibold mt-4 text-center max-sm:mt-24">Hello Again</h1>
-        <h1 className="mb-6 text-gray-400 font-semibold text-center">Enter you credentials to access your account</h1>
+        <h1 className="mb-3 text-3xl font-semibold mt-4 text-center max-sm:mt-24">Register Now</h1>
+        <h1 className="mb-6 text-gray-400 font-semibold text-center">You can sign up with your social account below</h1>
        <Formik 
 
         initialValues={{
           name:'',
-          email:''
+          email:'',
+          password:'',
+          confirmPassword:''
         }}
 
         validationSchema={Yup.object({
           name:Yup.string().required('Name can not be empty'),
-          email:Yup.string().email('Invalid email address').required('Email is required field')
+          email:Yup.string().email('Invalid email address').required('Email is required field'),
+          password:Yup.string().required('Password cannot be empty'),
+          confirmPassword:Yup.string().required('Confirm your password first')
         })}
 
         onSubmit={(values,{setSubmitting}) => {
@@ -46,12 +51,18 @@ const account = () => {
                   <div className='m-6 max-sm:my-2 max-sm:w-56'>
                       <Input Placeholder='Enter your name' Name='name' Type='text' />
                   </div>
+                  <div className='m-6 max-sm:my-2 max-sm:w-56'>
+                      <Input Placeholder='Enter your password' Name='password' Type='password' />
+                  </div>
+                  <div className='m-6 max-sm:my-2 max-sm:w-56'>
+                      <Input Placeholder='Enter your confirm password' Name='confirmPassword' Type='password' />
+                  </div>
               </div>
 
               <motion.button whileHover={{scale:1.1,opacity:0.7}} className="w-10/12 mt-10 ml-4 h-12 hover:bg-black font-bold bg-rose-500 text-slate-100 max-sm:mt-6 max-sm:ml-8" type="submit">Send Message</motion.button>
-              <h1 className='text-slate-500 text-center mt-4 mb-4'>Don't have account?<span className='text-rose-500'>
-                <Link href='/register'>
-                Register Now
+              <h1 className='text-slate-500 text-center mt-4 mb-4'>Already have an account?<span className='text-rose-500'>
+                <Link href='/account'>
+                    Login
                 </Link>
               </span></h1>
           </form>
@@ -60,7 +71,8 @@ const account = () => {
     </div>
     </div>
     </div>
+    </div>
   )
 }
 
-export default account
+export default Signup
---
+++ src/pages/register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../../components/Signup'
+
+const register = () => {
+  return (
+    <div>
+        <Signup />
+    </div>
+  )
+}
+
+export default register
